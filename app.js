a2fcdd166691a64d7c85a6f644bdef44ab8ea617
lubualo@gmail.com <lubualo@gmail.com>
2025-10-23 15:41:47 -0300

    Use EJS, add a MongoDb connection, morgan middleware for logging and styles

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const Blog = require("./models/blog")
+
 const app = express()
 
-app.listen(3000, "localhost", () => {
-    console.log("Server is running on http://localhost:3000")
-})
+// Connect to MongoDB
+const dbURI = "mongodb+srv://root:Test-1234@cluster0.s6buayy.mongodb.net/nodetuts?appName=Cluster0"
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000, "localhost", () => {
+        console.log("Server is running on http://localhost:3000")
+    }))
+    .catch((err) => console.log(err))
+
+app.set("view engine", "ejs")
+
+
+// middleware
+// log the request
+app.use(morgan("dev"))
+
+// serve static files
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.sendFile("views/index.html", { root: __dirname })
+    res.redirect("/blogs")
 })
 
 app.get("/about", (req, res) => {
-    res.sendFile("views/about.html", { root: __dirname })
+    res.render("about", {title: "About"})
+})
+
+app.get("/blogs", (req, res) => {
+    const blogs = Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.render("index", {title: "All Blogs", blogs: result})
+    })
+    .catch((err) => {
+        console.log("Error finding blogs:", err)
+        res.status(500).send("Error finding blogs: " + err.message)
+    })
+})
+
+app.get("/blogs/create", (req, res) => {
+    res.render("create", {title: "Create a new blog"})
 })
 
 app.get("/about-me", (req, res) => {
@@ -18,5 +51,5 @@ app.get("/about-me", (req, res) => {
 })
 
 app.use((req, res) => {
-    res.status(404).sendFile("views/404.html", { root: __dirname })
+    res.status(404).render("404", {title: "404"})
 })
